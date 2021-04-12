0dc5fd6c35987e58467560127d8d335547c17432
Andrew Churilo <AndrewChuriloMinsk@gmail.com>
2021-04-12 15:23:50 +0300

    Add empty project

---
+++ app/src/main/java/com/pdapps/compiler/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pdapps.compiler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
